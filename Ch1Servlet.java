0e4c0c657d5234f3d31425af8013730c653c4f4c
jadilovic <adilovic79@yahoo.com>
2019-10-12 23:02:29 +0200

    servlet commit

--- Ch1Servlet.java
+++ Ch1Servlet.java
@@ -1,4 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
-public class Ch1Servlet {
+import javax.servlet.http.*;
+import javax.servlet.*;
 
+
+public class Ch1Servlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		PrintWriter out = response.getWriter();
+		Date today = new Date();
+		out.println("<html>" + "<body>"
+		+ "<h1 align=center>HF\\’s Chapter1 Servlet</h1>"
+		+ "<br>" + today + "</body>" + "</html>");
+	}
+	
 }
---
+++ Primjeri/HelloWorldServlet.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorldServlet")
+
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// napraviti novi PrintWriter objekt za ispis odgovora
+		PrintWriter out = response.getWriter();
+
+		// odgovor za ispisati u browseru
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello World</h1>");
+		out.println("<p>Simple servlet</p>");
+		out.println("</body>");
+		out.println("</html>");
+
+		// zatvoriti PrintWriter
+		out.close();
+
+	}
+
+}
